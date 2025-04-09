13835a54ff15b28a667b0b6d4319c00cb2f95ea1
Parin Parikh <paringp@weblineapps.com>
2025-04-09 11:52:21 +0530

    corrected test file

--- investmentcalculator/src/app/investment-results/investment-results.component.spec.ts
+++ investmentcalculator/src/app/investment-results/investment-results.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InvestmentResultsComponent } from './investment-results.component';
 
-describe('InvestmentReslutsComponent', () => {
+describe('InvestmentResultsComponent', () => {
   let component: InvestmentResultsComponent;
   let fixture: ComponentFixture<InvestmentResultsComponent>;
 
