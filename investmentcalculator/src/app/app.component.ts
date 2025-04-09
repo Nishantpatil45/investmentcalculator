d420ded2ee6c06c0dc61847ddae402f19219dca0
Parin Parikh <paringp@weblineapps.com>
2025-04-09 11:50:19 +0530

    - Added investment result componenet to display investment calculations

--- investmentcalculator/src/app/app.component.ts
+++ investmentcalculator/src/app/app.component.ts
@@ -2,22 +2,33 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserInputComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserInputComponent,
+    InvestmentResultsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'investmentcalculator';
 
-  onCalculateInvestmentResults(data: {
-    initialInvestment: number;
-    expectedReturn: number;
-    duration: number;
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
     annualInvestment: number;
-  }) {
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, expectedReturn, duration, annualInvestment } =
       data;
     const annualData = [];
@@ -39,8 +50,6 @@ export class AppComponent {
       });
     }
 
-    //return annualData;
-
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
